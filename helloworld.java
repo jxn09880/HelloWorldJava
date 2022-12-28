117912543d5582c1f1de2933d75f807447e589bb
root <root@ip-172-31-82-84.ec2.internal>
2022-12-28 19:43:15 +0000

    message add

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public class helloworld {
+
+    public static void main(String[] args) {
+
+        // Prints "hello, world" in the terminal window.
+        System.out.println("hello, world");
+    }
+
+}
